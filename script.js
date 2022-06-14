af97bfb2c1ac814ca36ea327a8b5aa49785bef90
PedroMarMol <martosmoleropedro@gmail.com>
2022-06-14 20:18:34 +0200

    bg image from search (commented bc couldnt get the image searching api

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
+        // document.body.style.backgroundImage = "url("url-source" + name)"; 
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
